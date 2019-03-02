8ddeffa6f5904453b94dfc497b9287a638833910
candicei <candicejxi@gmail.com>
2019-03-02 11:20:40 -0800

    made changes to avoid enemy snake heads!

--- app/main.py
+++ app/main.py
@@ -2,284 +2,335 @@ import json
 import os
 import random
 import bottle
-import math
+import numpy as np
 
 from api import ping_response, start_response, move_response, end_response
 
-class Node:
-    def __init__ (self, coordinates, f_cost = None, visited = None, parent = None, isSnake = None, wall = None):
-        self.f_cost = None
-        self.coordinates = coordinates ##{[x][y]}
-        self.visited = False
-        self.parent = None
-        self.isSnake = None
-        self.wall = None
-        self.h_cost = None
 
+# A Star Implementation from: https://medium.com/@nicholas.w.swift/easy-a-star-pathfinding-7e6689c7f7b2
+class Node():
+    """A node class for A* Pathfinding"""
 
-def distance(food, head):
-    return math.sqrt((food['x'] - head['x'])**2 + (food['y'] - head['y'])**2)
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
 
-def f_distance(node, target):
-    return math.sqrt((node.coordinates['x'] - target['x'])**2 + (node.coordinates['y'] - target['y'])**2)
+        self.g = 0
+        self.h = 0
+        self.f = 0
+
+    def __eq__(self, other):
+        return self.position == other.position
+
+    
+#A* Algorithm    
+def astar(maze, start, end):
+    """Returns a list of tuples as a path from the given start to the given end in the given maze"""
+
+    # Create start and end node
+    start_node = Node(None, start)
+    start_node.g = start_node.h = start_node.f = 0
+    end_node = Node(None, end)
+    end_node.g = end_node.h = end_node.f = 0
+
+    # Initialize both open and closed list
+    open_list = []
+    closed_list = []
+
+    # Add the start node
+    open_list.append(start_node)
+
+    # Loop until you find the end
+    while len(open_list) > 0:
+
+        # Get the current node
+        current_node = open_list[0]
+        current_index = 0
+        for index, item in enumerate(open_list):
+            if item.f < current_node.f:
+                current_node = item
+                current_index = index
+
+        # Pop current off open list, add to closed list
+        open_list.pop(current_index)
+        closed_list.append(current_node)
+
+        # Found the goal
+        if current_node == end_node:
+            path = []
+            current = current_node
+            while current is not None:
+                path.append(current.position)
+                current = current.parent
+            return path[::-1] # Return reversed path
+
+        # Generate children
+        children = []
+        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]: # Adjacent squares
+
+            # Get node position
+            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
+
+            # Make sure within range
+            if node_position[0] > (len(maze) - 1) or node_position[0] < 0 or node_position[1] > (len(maze[len(maze)-1]) -1) or node_position[1] < 0:
+                continue
+
+            # Make sure walkable terrain
+            if maze[node_position[0]][node_position[1]] != 0:
+                continue
+
+            # Create new node
+            new_node = Node(current_node, node_position)
+
+            # Append
+            children.append(new_node)
+
+        # Loop through children
+        for child in children:
+
+            # Child is on the closed list
+            for closed_child in closed_list:
+                if child == closed_child:
+                    continue
+
+            # Create the f, g, and h values
+            child.g = current_node.g + 1
+            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
+            child.f = child.g + child.h
+
+            # Child is already in the open list
+            for open_node in open_list:
+                if child == open_node and child.g > open_node.g:
+                    continue
+
+            # Add the child to the open list
+            open_list.append(child)
+    
+    
+    
+#Returns the maze filled with zeros
+def returnMaze(data):
+    boardx = data['board']['height']
+    boardy = data['board']['width']
+    maze = np.zeros((boardx,boardy),dtype=int)
+    return maze
+
+#Gives the (int) x and y position of our OWN snake in two lists (x, y)
+def getSelfPos(data):
+    snake = data['you']['body']
+    x = [snakePos['x'] for snakePos in snake]
+    y = [snakePos['y'] for snakePos in snake]
+    return x,y
+
+# return the current head postion of OWN snake in (int) as a tuple
+def getSelfHeadPos(data):
+    xself,yself = getSelfPos(data)
+    x = xself[0]
+    y = yself[0]
+    return (x,y)
+
+#Gives the entire position of snakes 
+def enemyAllPos(data):
+    numEnemies = enemyCount(data)
+    xx = [] #initializing
+    yy = [] #initializing
+    if numEnemies > 0:
+        #looping to find an appended list of x and y coordinates of all enemy snakes
+        for s in range(numEnemies):
+            x,y = enemy1Pos(data,s+1)
+            xx += x
+            yy += y
+    return xx,yy
+
+#Gives the x,y as a list of int values of the a snake given 'numSnake' which starts at 0  
+def enemy1Pos(data,numSnake):
+    snake = data['board']['snakes'][numSnake]['body']
+    x = [snakePos['x'] for snakePos in snake]
+    y = [snakePos['y'] for snakePos in snake]
+    return x,y
+
+#Gives the position of surrounding position of enemies heads and their potential 'next step'
+def enemySurroundHeadPos(data):
+    numEnemies = enemyCount(data)
+    xx = [] #initializing
+    yy = [] #initializing
+    direction_x = [0, 0, -1, 1] # referring to up, down, left, right
+    direction_y = [-1, 1, 0, 0] # referring to up, down, left, right
+    
+    if numEnemies > 0:
+        #looping to find the 4 locations surrounding the head
+        for s in range(numEnemies+1):
+            x,y = enemy1Pos(data,s)
+        
+            headx = x[0]
+            heady = y[0]
+#            print('head of enemy')
+#            print((headx,heady))
+#            print('head of  own')
+#            print(getSelfHeadPos(data))
+            if not((headx,heady) == getSelfHeadPos(data)):
+#                print('got that the heads are different')
+                #looping through the directions
+                for rr in range(4):
+                    dir = [(headx)+direction_x[rr],(heady+direction_y[rr])] #find the 4 locations an enemie's head can move into in the next step
+                    if (int(dir[0]) in range(0,(data['board']['height']-1)) and int(dir[1]) in range(0,(data['board']['width']-1))): #making sure the ranges are not out of bounds for x or y
+                        xx+= [dir[0]]
+                        yy+= [dir[1]]
+    
+#    print('surrounding head:')
+#    print(xx,yy)
+    return xx,yy
+
+#Gives the number of enemies still alive
+def enemyCount(data):
+    numEnemies = sum(1 for enemies in data['board']['snakes']) -1 # minus one because the board counts itself in "snakes"
+    return numEnemies
+
+#Finding location of fruits
+def fruitLoc(data):
+    snake = data['board']['food']
+    x = [snakePos['x'] for snakePos in snake]
+    y = [snakePos['y'] for snakePos in snake]
+    return x,y
+
+#Finding nearest fruit to head. Returns position as a tuple (int) x,y
+def closestFruit(data):
+    fx,fy = fruitLoc(data) #getting x,y of all fruits
+    hx,hy = getSelfHeadPos(data) #getting position of head
+    distx = np.array(fx) - hx
+    disty = np.array(fy) - hy
+    distx *= distx #calculating the square
+    disty *= disty #calculating the square
+    dist = distx+disty #calculating distance (without taking sqrt)
+    indexMin = np.argmin(dist) #checked: only returns one index which is good
+    return (fx[indexMin],fy[indexMin])
+
+#provides the string direction: 'up','down','left','right' from the path
+def returnDirection(path):
+    arrayDirection = np.subtract(path[1],path[0])
+    if (arrayDirection == ([0,-1])).all():
+        direction = 'up'
+    elif (arrayDirection == ([0,1])).all():
+        direction = 'down'
+    elif (arrayDirection == ([-1,0])).all():
+        direction = 'left'
+    elif (arrayDirection == ([1,0])).all():
+        direction = 'right'
+    else:
+        direction = 'right'
+        print('random direction chosen!')
+    return direction 
+
 
 @bottle.route('/')
 def index():
     return '''
-        Battlesnake documentation can be found at
-        <a href="https://docs.battlesnake.io">https://docs.battlesnake.io</a>.
-        '''
+    Battlesnake documentation can be found at
+       <a href="https://docs.battlesnake.io">https://docs.battlesnake.io</a>.
+    '''
 
 @bottle.route('/static/<path:path>')
 def static(path):
     """
-        Given a path, return the static file located relative
-        to the static folder.
-        
-        This can be used to return the snake head URL in an API response.
-        """
+    Given a path, return the static file located relative
+    to the static folder.
+
+    This can be used to return the snake head URL in an API response.
+    """
     return bottle.static_file(path, root='static/')
 
 @bottle.post('/ping')
 def ping():
     """
-        A keep-alive endpoint used to prevent cloud application platforms,
-        such as Heroku, from sleeping the application instance.
-        """
+    A keep-alive endpoint used to prevent cloud application platforms,
+    such as Heroku, from sleeping the application instance.
+    """
     return ping_response()
 
 @bottle.post('/start')
 def start():
     data = bottle.request.json
-    
+    print(" ")
+    print("---------------------")
+    print("New Game")
+    print(data)
     """
-        TODO: If you intend to have a stateful snake AI,
-        initialize your snake state here using the
-        request's data if necessary.
-        """
-    print(json.dumps(data))
-    
+    TODO: If you intend to have a stateful snake AI,
+            initialize your snake state here using the
+            request's data if necessary.
+    """
+#    print(json.dumps(data))
+
+    # Obtaining board height and width!
+    boardx = data['board']['height'] 
+    boardy = data['board']['width']
+    print(boardx)
+    print(boardy)
+
     color = "#00FF00"
-    
+
     return start_response(color)
-
-def f_cost(data, node, target):
-    h_cost = node.parent.h_cost
-    h_cost += 1
-    node.h_cost = h_cost
-    ourHead = data['you']['body'][0]
-    f_cost = h_cost + f_distance(node, target)
-    return f_cost
-
-def createNeighbors(data, node, target):
-    neighbors = []
-    ## Create right neighbor
-    if(node.coordinates['x'] + 1 != data['board']['width']):
-        new_coords = node.coordinates
-        new_coords['x'] += 1
-        tmp_node = Node(new_coords)
-        tmp_node.parent = node
-        tmp_cost = f_cost(data, tmp_node, target)
-        tmp_node.f_cost = tmp_cost
-        neighbors.append(tmp_node)
-    
-    ## Create left neighbor
-    if(node.coordinates['x'] - 1 != 0):
-        new_coords = node.coordinates
-        new_coords['x'] -= 1
-        tmp_node = Node(new_coords)
-        tmp_node.parent = node
-        tmp_cost = f_cost(data, tmp_node, target)
-        tmp_node.f_cost = tmp_cost
-        neighbors.append(tmp_node)
-    
-    ## Create top neighbor
-    if(node.coordinates['y'] - 1 != 0):
-        new_coords = node.coordinates
-        new_coords['y'] -= 1
-        tmp_node = Node(new_coords)
-        tmp_node.parent = node
-        tmp_cost = f_cost(data, tmp_node, target)
-        tmp_node.f_cost = tmp_cost
-        neighbors.append(tmp_node)
-    
-    ## Create bottom neighbor
-    if(node.coordinates['y'] + 1 != data['board']['height']):
-        new_coords = node.coordinates
-        new_coords['y'] += 1
-        tmp_node = Node(new_coords)
-        tmp_node.parent = node
-        tmp_cost = f_cost(data, tmp_node, target)
-        tmp_node.f_cost = tmp_cost
-        neighbors.append(tmp_node)
-    
-    return neighbors
-
-def isValid(data, node):
-    valid = True
-    
-    snakes = data['board']['snakes']
-    for i in range(len(snakes)):
-        for j in range(len(snakes[i]['body'])):
-            if(node.coordinates == snakes[i]['body'][j]):
-                valid = False
-                return valid
-
-## TO DO: Check nodes around enemy snake heads
-
-ourBody = data['you']['body']
-for k in range(len(ourBody)):
-    if(node.coordinates == ourBody[k]):
-        valid = False
-            return valid
-
-return valid
-
-def A_Star(data):
-    ourHead = data['you']['body'][0]
-    
-    open_list = []
-    closed_list = []
-    
-    
-    node = Node(ourHead)
-    node.f_cost = 0
-    node.h_cost = 0
-    node.visited = True
-    open_list.append(node)
-    
-    target_food = data['board']['food'][0]
-    min_distance = distance(data['board']['food'][0], ourHead)
-    
-    for i in range(len(data['board']['food'])):
-        if(distance(data['board']['food'][i], ourHead) < min_distance):
-            min_distance = distance(data['board']['food'][i], ourHead)
-            target_food = data['board']['food'][i]
-    ##print("right before while loop")
-    for i in range(50):
-        
-        
-        ## lowest_fcost is a node
-        lowest_fcost = open_list[0]
-        for i in range(len(open_list)):
-            if open_list[i].f_cost < lowest_fcost.f_cost:
-                print(lowest_fcost.f_cost)
-                lowest_fcost = open_list[i]
-
-        lowest_fcost.visited = True
-        closed_list.append(lowest_fcost)
-        open_list.remove(lowest_fcost)
-
-curr_coords = lowest_fcost.coordinates
-    if curr_coords['x'] == target_food['x'] and curr_coords['y'] == target_food['y']:
-        print("we got the lowest cost yayayaya")
-        print(lowest_fcost)
-        return lowest_fcost
-        
-        neighbors = createNeighbors(data, lowest_fcost, target_food)
-        
-        for j in range(len(neighbors)):
-            if isValid(data, neighbors[j]) or neighbors[j] not in closed_list:
-                already_exists = False
-                for i in range(len(open_list)):
-                    if open_list[i].coordinates == neighbors[j].coordinates:
-                        already_exists = True
-                        if open_list[i].f_cost > neighbors[j].f_cost:
-                            open_list.remove(open_list[i])
-                            open_list.append(neighbors[j])
-                if already_exists == False:
-                    open_list.append(neighbors[j])
-
-
-def direction(node, ourHead):
-    direction = ''
-    tempnode = node
-    while(tempnode.parent != ourHead):
-        tempnode = tempnode.parent
-    
-    ## Check Right
-    temp_r = ourHead
-    temp_r[0] += 1
-    if tempnode.coordinates == temp_r:
-        direction = 'right'
-    
-    ## Check Left
-    temp_l = ourHead
-    temp_l[0] -= 1
-    if tempnode.coordinates == temp_l:
-        direction = 'left'
-    
-    ## Check Up
-    temp_u = ourHead
-    temp_u[1] -= 1
-    if tempnode.coordinates == temp_u:
-        direction = 'up'
-    
-    ## Check Down
-    temp_d = ourHead
-    temp_d[1] += 1
-    if tempnode.coordinates == temp_d:
-        direction = 'down'
-    
-    return direction
 
 
 @bottle.post('/move')
 def move():
-    ##print("1************************************\n\n\n\n")
-    
     data = bottle.request.json
-    
     """
-        TODO: Using the data from the endpoint request object, your
-        snake AI must choose a direction to move in.
-        """
-    ##print(json.dumps(data))
+    TODO: Using the data from the endpoint request object, your
+            snake AI must choose a direction to move in.
+    """
+#    print('--')
+    numEnemies = enemyCount(data)
+    #print(numEnemies)
+#    print(data['turn'])
     
+
+    #obtaining 'maze' for astar
+    maze = returnMaze(data) #obtaining maze (size)
     
+    enemyXLoc, enemyYLoc = enemyAllPos(data) #obtaing locations of enemies on maze
+    ownXLoc, ownYLoc = getSelfPos(data) #obtaining own snake location on maze
+    enemyHeadMoveX, enemyHeadMoveY = enemySurroundHeadPos(data) #obtaining locations of the potential location of future enemy heads 
+#    print('enemy head 4 locss')
+    print(enemyHeadMoveX, enemyHeadMoveY)
     
+    maze[enemyXLoc, enemyYLoc] = 1 #marking locations of other snakes on maze
+    maze[ownXLoc, ownYLoc] = 1 #marking self location on maze
+    maze[enemyHeadMoveX, enemyHeadMoveY] = 1 #marking locations of potential enemy sneakhead movements on maze
+
+    #obtaining 'start' for astar 
+    start = getSelfHeadPos(data) #using current head location as 'start'
+#    print(start)
     
-    directions = ['up', 'down', 'left', 'right']
-    #direction = random.choice(directions)
-    boardheight = data['board']['height']
-    boardwidth = data['board']['width']
+    #obtaining 'end' for astar 
+    end = closestFruit(data)
+#    print(end)
     
-    ourHead = data['you']['body'][0]
+    #calculating astar for the shortest path
+    path = astar(maze, start, end)
+#    print('path:')
+#    print(path)
     
-    rightTopCorner = ourHead['y']==0 and ourHead['x']==boardwidth-1
-    origin = ourHead['y']==0 and ourHead['x']==0
-    leftBottomCorner = ourHead['x']==0 and ourHead['y']==boardheight-1
-    rightBottomCorner = ourHead['x']==boardwidth-1 and ourHead['y']==boardheight-1
-    rightWall = ourHead['x']==boardwidth-1
-    bottomWall = ourHead['y']==boardheight-1
-    leftWall = ourHead['x']==0
-    topWall = ourHead['y']==0
+    #determining direction
+    direction = (returnDirection(path))
+#    print('direction:')
+#    print(direction)
+#    print(data)
     
-    node = A_Star(data)
-    print("After astar ************************************\n\n\n\n")
-    
-    direction_bigd = direction(node, ourHead)
-    print("************************************\n\n\n\n")
-    print(direction_bigd)
-    return move_response(direction_bigd)
+    return move_response(direction)
 
 
 @bottle.post('/end')
 def end():
     data = bottle.request.json
-    
+    print(data)
+
     """
-        TODO: If your snake AI was stateful,
+    TODO: If your snake AI was stateful,
         clean up any stateful objects here.
-        """
-    
-    # x=data['turn']
-    
-    
-    # print(x)
-    
-    ##print(json.dumps(data))
-    
+    """
+#    print(json.dumps(data))
+
     return end_response()
 
 # Expose WSGI app (so gunicorn can find it)
@@ -287,26 +338,8 @@ application = bottle.default_app()
 
 if __name__ == '__main__':
     bottle.run(
-               application,
-               host=os.getenv('IP', '0.0.0.0'),
-               port=os.getenv('PORT', '8080'),
-               debug=os.getenv('DEBUG', True)
-               )
-
-def move():
-    data = bottle.request.json
-    
-    """
-        TODO: Using the data from the endpoint request object, your
-        snake AI must choose a direction to move in.
-        """
-    ##print(json.dumps(data))
-    
-    directions = ['up', 'down', 'left', 'right']
-    direction = random.choice(directions)
-    
-    return move_response(direction)
-
-
-
-
+        application,
+        host=os.getenv('IP', '0.0.0.0'),
+        port=os.getenv('PORT', '8080'),
+        debug=os.getenv('DEBUG', True)
+    )
